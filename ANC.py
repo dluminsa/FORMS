5dd827107efc881e729098d5e7fd608893c4632b
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-06-12 21:22:38 -0700

    Update ANC.py

--- ANC.py
+++ ANC.py
@@ -261,7 +261,7 @@ if submit:
 date = datetime.now().date()
 formatted = date.strftime("%d-%m-%Y")
 st.write(formatted)
-df = pd.DataFrame([{'DATE': formatted, 
+df = pd.DataFrame([{ 
                     'CLUSTER': cluster,
                     'DISTRICT':district,
                     'FACILITY' : facility,
