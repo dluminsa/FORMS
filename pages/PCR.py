57f5deac968d41b13c65272b220e961af6a406b0
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-08-22 16:13:01 +0300

    Update PCR.py

--- pages/PCR.py
+++ pages/PCR.py
@@ -231,11 +231,11 @@ if cohort=='YES':
             if search == 'ART NO':
                 #st.write('**IF NOT FOUND, THEN EITHER SEARCH HER BY UNIQUE ID OR FIRST REGISTER HER**')
                 if n == 0:
-                    st.write(f'**FOUND NO MOTHER FOR {facility} IN THE DATABASE**')
-                    st.write(f'**SEARCH HER BY ID OR REGISTER HER FIRST**')
+                    st.write(f'FOUND NO MOTHER FOR {facility} IN THE DATABASE**')
+                    st.write('**SEARCH HER BY ID OR REGISTER HER FIRST**')
                     st.stop()
                 else:
-                    st.write('**FOUND {n} IDS FOR MOTHERS IN {facility} ANC DATA BASE**')
+                    st.write(f'**FOUND {n} IDS FOR MOTHERS IN {facility} ANC DATA BASE**')
                     st.write('**IF NOT FOUND, THEN EITHER SEARCH HER BY UNIQUE ID OR FIRST REGISTER HER**')
 
             
