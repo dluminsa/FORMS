07653a28512890d59a69439188afa6b624171202
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-09-25 20:43:27 +0300

    Update TESTING.py

--- pages/TESTING.py
+++ pages/TESTING.py
@@ -151,7 +151,7 @@ client = gspread.authorize(credentials)
 
 # Open the Google Sheet by URL
 spreadsheet_url = secrets["spreadsheet"]
-#spreadsheet_url =r"https://docs.google.com/spreadsheets/d/1q7oeVZ6UNhDxAj24qk3hLFLX2d0pGfxCsM9XycocqYQ/edit?gid=777763999#gid=777763999"
+spreadsheet_url ="https://docs.google.com/spreadsheets/d/1q7oeVZ6UNhDxAj24qk3hLFLX2d0pGfxCsM9XycocqYQ/edit?gid=777763999#gid=777763999" '
 spreadsheet = client.open_by_url(spreadsheet_url)
 
 # Access the worksheet named 'PMTCTB'
@@ -166,7 +166,7 @@ sheet = spreadsheet.worksheet("PMTCTB")
 # ]
 
 # Append the rows to the end of the sheet
-sheet.append_rows(rows_to_append, value_input_option='RAW')
+#sheet.append_rows(rows_to_append, value_input_option='RAW')
 
 file = r'DISTRICT.csv'
 dis = pd.read_csv(file)
