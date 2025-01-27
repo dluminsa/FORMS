78d4717b219ffd493cd2caafdf85ceb87130f0ef
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-27 19:22:28 +0300

    Update DELIVERY.py

--- pages/DELIVERY.py
+++ pages/DELIVERY.py
@@ -8,6 +8,7 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
+sssssss
 
 CLUSTER = {
     "KALANGALA": ["KALANGALA"],
